40b5ce59bf2e0506f20a6ae696fad357a456abb0
JunryJunee <zjyzhongjunyu@163.com>
2021-09-10 14:00:09 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
@@ -64,7 +65,11 @@ class User extends Authenticatable
     }   
 
     public function feed(){
-        return $this->statuses()->orderBy('created_at','desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                              ->with('user')
+                              ->orderBy('created_at', 'desc');
     }
 
     public function followers()
@@ -98,4 +103,7 @@ class User extends Authenticatable
     {
         return $this->followings->contains($user_id);
     }
+
+
+
 }
